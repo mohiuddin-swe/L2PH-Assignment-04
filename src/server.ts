f0e6c46c5eb04e04ed23d0d998ad2992adbe8d2a
Mohiuddin <mohiuddin.swe@gmail.com>
2026-07-29 02:57:31 +0600

    prisma setup, env setup and express built in middleware setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from './app';
+import {prisma} from './lib/prisma';
+import config from './config';
+
+const PORT = config.port;
+import "dotenv/config";
+
+async function main()  {  
+    try {
+        // await prisma.$connect();
+        console.log('Connected to the database');
+       app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting the server:', error);
+        // await prisma.$disconnect();
+        process.exit(1);
+    }}
+
+    main();
